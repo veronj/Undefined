1656985e638eb290b62690cdf8d653105551614b
veronj <veron_j@etna-alternance.net>
2018-12-11 10:12:29 +0100

    nearest display void

--- resources/views/userView.blade.php
+++ resources/views/userView.blade.php
@@ -9,9 +9,15 @@
             <div class="col-md-4">
             <div class="thumbnail">
                 <div class="caption">
-                <h3>{{ $sector->name }}</h3> 
-                <h3>{{ $sector->img }}</h3>
-                <a href="{{ route('userMove', ['x_position' => $sector->x_position, 'y_position' => $sector->y_position]) }}"><img src="/img/{{ $sector->img }}.png"></a>
+                
+                @if($sector != null) 
+                    <h3>{{ $sector->name }}</h3> 
+                    <a href="{{ route('userMove', ['x_position' => $sector->x_position, 'y_position' => $sector->y_position]) }}"><img src="/img/{{ $sector->img }}.png"></a>
+                @else
+                    <h3></h3>    
+                <a href="#"><img src="/img/void.png"></a>
+                @endif
+                
                 
                 </div>
             </div>
